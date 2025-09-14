db5c679fd4fc73f4e752bdf0e98c762cccee1a47
dnlsyfq <danialsyafiq@outlook.my>
2025-09-15 00:55:56 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,9 @@
+let userLoggedIn = true;
+
+if (userLoggedIn) {
+    console.log("Welcome back! Proceeding to checkout.");
+}
+
+if(!userLoggedIn) {
+    console.log("Please log in to continue to checkout.");
+}
